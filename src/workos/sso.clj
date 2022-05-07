fdba5b0de34ac41cc010d3276bfdc20870514a2e
Avelino <avelinorun@gmail.com>
2022-05-07 14:49:53 -0300

    sso/redirect-url

--- src/workos/sso.clj
+++ src/workos/sso.clj
@@ -5,10 +5,19 @@
 (defn get-profile-token
   "Fetch the profile details and access token for the authenticated SSO user"
   [code]
-  (.getProfileAndToken (.-sso (WorkOS. (:secret config/apikey))) code (:client config/apikey)))
+  (.getProfileAndToken (.-sso (WorkOS. (:secret config/apikey)))
+                       code (:client config/apikey)))
 
 (defn get-profile
   "Fetch the profile details via provided access token"
   [access-token]
   (.getProfile (.-sso (WorkOS. (:secret config/apikey))) access-token))
 
+(defn redirect-url
+  "Generate an Oauth2 authorization URL where your users will
+   authenticate using the configured SSO Identity Provider"
+  [connection-id
+   & {:keys [redirect-url]
+      :or {redirect-url config/redirect-url}}]
+  (.getAuthorizationUrl (.-sso (WorkOS. (:secret config/apikey)))
+                        connection-id redirect-url))
